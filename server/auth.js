ee0df3c365f1af42dc3b34cbc3315afbc914f804
Vitali Rzheutski <vitalirzheutski@macbook-pro.myfiosgateway.com>
2021-11-15 12:49:10 -0500

    Login Route

--- server/auth.js
+++ server/auth.js
@@ -2,4 +2,20 @@ const router = require('express').Router()
 const {User} = require('./db')
 module.exports = router
 
-// auth routes go below!
+//the full route will be http://localhost:3000/auth/login
+router.put('/login',async(req,res,next)=>{
+    try{
+        console.log('req.params:',req.body)
+        const user = await User.findOne(req.body,{
+            where:{
+                email:req.params.email,
+                password:req.params.password
+            }
+        })
+        console.log('USER:',user)
+        if(!user)res.sendStatus(401);
+        res.sendStatus(200)
+    }catch(error){
+        next(error)
+    }
+})
