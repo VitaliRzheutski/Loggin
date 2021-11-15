c49f0a197ff55861362bc19675840faa208a095b
Vitali Rzheutski <vitalirzheutski@macbook-pro.myfiosgateway.com>
2021-11-15 12:55:32 -0500

    login thunk creator

--- client/store.js
+++ client/store.js
@@ -1,12 +1,24 @@
 import { createStore, applyMiddleware } from 'redux'
 import loggerMiddleware from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
+import axios from 'axios'
 //action type
 const GET_USER = 'GET_USER';
 //action creator
 export const gotUser = (user) => {
   return {
     type: GET_USER, user
+  }
+}
+//thunk creator
+export const loginThunk = (formData)=>{
+  return async (dispatch) =>{
+    try{
+      const {data} = await axios.put('/auth/login',formData);
+      dispatch(gotUser(data))
+    }catch(error){
+      console.log(error)
+    }
   }
 }
 const initialState = {
